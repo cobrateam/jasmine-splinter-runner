2be7275d61e5a2aacc55b59e50a0096f1c42346e
Francisco Souza <francisco@franciscosouza.net>
2011-06-21 13:17:03 -0300

    Basic distribution structure for PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from jasmine_runner import __version__
+
+README = open('README.rst').read()
+
+setup(name='jasmine-splinter-runner',
+      version=__version__,
+      description='jasmine runne based on splinter',
+      long_description=README,
+      author='CobraTeam',
+      author_email='francisco@franciscosouza.net',
+      packages=find_packages(),
+      include_package_data=True,
+      test_suite='nose.collector',
+      install_requires=['splinter'],
+      entry_points = {
+          'console_scripts' : [
+              'jasmine-splinter = jasmine_runner.commands:main',
+          ]
+      },
+     )
+
